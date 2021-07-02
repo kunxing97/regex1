6d723df38e0fb4b90064408a9ea95111d1b447cf
kunxing97 <kxing97@gmail.com>
2021-07-02 16:22:07 +0000

    png working

--- regrex1_in_R.r
+++ regrex1_in_R.r
@@ -3,18 +3,18 @@ mydata
 xval = mydata$x
 yval = mydata$y
 
+png("r_orig.png")
 plot(xval, yval, main="Scatterplot in R")
-png('r_orig.png')
+dev.off()
 
 linearmodel <- lm(yval ~ xval, data=mydata) 
 print(linearmodel)
 
 predict(linearmodel, mydata)
 
-plot(xval, yval ,col = "blue",main = "Linear Regression in R",abline(linearmodel), cex = 1.3,pch = 16,xlab = "x",ylab = "y")
 png('r_Im.png')
-
-
+plot(xval, yval ,col = "blue",main = "Linear Regression in R",abline(linearmodel), cex = 1.3,pch = 16,xlab = "x",ylab = "y")
+dev.off()
 
 print("here")
 
